a115672d5a8c6ff7a01ab039d8db17a7b7d3de9d
Humphrey Xu <xuhumphrey@gmail.com>
2019-03-26 21:42:58 -0500

    Add files via upload

---
+++ class7/NestedLoops.java
@@ -0,0 +1,15 @@
+package class5;
+
+public class NestedLoops {
+
+	public static void main(String[] args) {
+		// We can use loops inside of other loops
+		// to create multidimensional nested loops.
+		
+		for(int i = 0; i <= 10; i++) {
+			for(int j = 0; j < 10; j++) {
+				System.out.println("i = " + i + " | j = " + j);
+			}
+		}
+	}
+}
---
+++ class7/NestedLoops_ex1.java
@@ -0,0 +1,15 @@
+package class5;
+
+public class NestedLoops_ex1 {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 6; i++) {
+			System.out.println("");
+			for(int j = 0; j < 6; j++) {
+				System.out.print("*");
+			}
+		}
+
+	}
+
+}
